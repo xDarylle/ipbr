965b20a74e33977b4c3932b3ef8a817d6c460c59
torisutanjon <giraotristan@gmail.com>
2022-02-03 15:21:19 +0800

    done background cancel button and delete input path and input counter

--- scripts/main_gui.py
+++ scripts/main_gui.py
@@ -31,9 +31,7 @@ def background_panel_gui():
     # create button widgets
     tk.Button(background_panel, height=20, width=20, bd=0, image=add_background_icon, cursor="hand2",
               command=lambda: add_background(background_panel)).place(relx=0.05, rely=0.025)
-    tk.Button(background_panel, height=0, width=0, bd=0, text="Add Background", font=("Roboto", 15), fg="#CDC9C9",
-              bg="#383d3a", cursor="hand2", command=(lambda: add_background(background_panel))).place(relx=0.15,
-                                                                                                      rely=0.015)
+    tk.Button(background_panel, height = 2, width = 10, text = "Close", font = ("Roboto", 12), cursor = "hand2", fg="white", bg="#ba6032", command = background_panel.destroy).place(relx = 0.70, rely = 0.015)
     # recreate the image gallery with current image and panel as paramenter
     for img in backgrounds_array:
         create_background_gallery(img, background_panel)
@@ -112,10 +110,10 @@ def open_settings():
     temp = tk.BooleanVar()
     # create variable for checkbox  with bollean as datatype
 
-    setting_panel = tk.Frame(mainwindow, height=720, bd=2, width=350, relief="groove", bg="#161010")
+    setting_panel = tk.Frame(mainwindow, height=720, width=350, bg="#161010")
     setting_panel.place(relx=0.72, rely=0)
     tk.Button(setting_panel, height=2, width=10, text="Cancel", font=("Roboto", 11), fg="white", bg="#ba6032",
-              cursor="hand2", command=setting_panel.destroy).place(relx=0.65, rely=0.025)
+              cursor="hand2", command=setting_panel.destroy).place(relx = 0.70, rely=0.015)
     tk.Button(setting_panel, height=2, width=25, text="Apply Changes", font=("Roboto", 11), fg="white", bg="#127DF4",
               cursor="hand2", command=lambda: [
             save_settings(height_error_label, width_error_label, output_error_label, setting_panel,  ifcheck_var)]).place(
@@ -197,7 +195,7 @@ def save_settings(height_error_label, width_error_label, output_error_label , se
 
     if ifcheck_var is True:
         try:
-            height_var = int(height_entry_var)
+            height_var = int(height_entry_var.get())
             height_error_label.configure(text="")
             height_error = False
             # check if height input is 512 or higher
@@ -213,7 +211,7 @@ def save_settings(height_error_label, width_error_label, output_error_label , se
             height_error_label.configure(text="Numbers Only (Integers)")
             height_error = True
         try:
-            width_var = int(width_entry_var)
+            width_var = int(width_entry_var.get())
             width_error_label.configure(text="")
             width_error = False
             # check if height input is 512 or higher
@@ -275,33 +273,15 @@ def drop_inside_list_box(event):
     global input_folder_path
     #assign it with data from event
     input_folder_path = event.data
-    #configure the label widget to display path text in the gui
-    input_folder_label.configure(text = input_folder_path)
-    #call counter function and pass the path
-    file_counter_handler(input_folder_path)
 
 def get_input_handler():
     #access essential variable
     global input_folder_path
     #assign it with data from event
     input_folder_path = filedialog.askdirectory(initialdir = "/Desktop",title = "Select Input Path")
-    #configure the label widget to display path text in the gui
-    input_folder_label.configure(text = input_folder_path)
-    #call counter function and pass the path
-    file_counter_handler(input_folder_path)
 
-def file_counter_handler(filepath):
-    #initialise a counter variable
-    i = 0
-    #looping every file inside the path
-    for name in os.listdir(filepath):
-        #check if file is an image or not
-        if imghdr.what(os.path.join(filepath, name)) == "jpeg" or imghdr.what(os.path.join(filepath, name)) == "jpg" or imghdr.what(os.path.join(filepath, name)) == "png":
-            #if file is an image increase counter var by 1
-            i += 1
-    #configure label widget to display file count to gui
-    input_counter_label.configure(text = i)
-
+def input_gallery_gui():
+    input_gallery_frame = tk.Frame()
 
 # start of main gui creationg with TkinterDnD wrapper
 mainwindow = TkinterDnD.Tk()
@@ -334,7 +314,6 @@ width_entry_var = tk.StringVar()
 height_entry_var.set(str(height_var))
 width_entry_var.set(str(width_var))
 temp_output_loc = output_loc
-input_count = 0
 yindex = 0.1
 im_index = 0
 
@@ -352,19 +331,13 @@ mainwindow.configure(bg = "#2C2B2B")
 mainwindow.resizable(False, False)
 
 #create main window widgets
-tk.Label(mainwindow, text = "Input Path: ", font = ("Roboto", 12), fg = "#D6D2D2", bg = "#2C2B2B").place(relx= 0.02, rely=0.02)
-input_folder_label = tk.Label(mainwindow,  font = ("Rboto", 12), fg = "#D6D2D2", bg = "#2C2B2B")
-input_folder_label.place(relx= 0.085, rely= 0.02)
-tk.Label(mainwindow, text = "Input Count", font = ("Roboto", 12), fg = "#D6D2D2", bg = "#2C2B2B").place(relx= 0.4, rely= 0.02)
-input_counter_label = tk.Label(mainwindow,  font = ("Roboto", 12), fg = "#D6D2D2", bg = "#2C2B2B")
-input_counter_label.place(relx= 0.475, rely= 0.02)
-foreground_input_list_box = tk.Listbox(mainwindow, selectmode= tk.SINGLE, width = 155, height = 31, bg = "#2C2B2B", bd = 1, relief = "groove")
+foreground_input_list_box = tk.Listbox(mainwindow, selectmode= tk.SINGLE, width = 155, height = 50, bg = "#2C2B2B", bd = 1, relief = "groove")
 foreground_input_list_box.drop_target_register(DND_FILES)
 foreground_input_list_box.dnd_bind("<<Drop>>", drop_inside_list_box)
-foreground_input_list_box.place(relx= 0, rely=0.3)
-tk.Label(foreground_input_list_box, text= "Drop Input Folder Here", font = ("Roboto", 14), fg = "#D6D2D2", bg = "#2C2B2B").place(relx= 0.40, rely = 0.35)
-tk.Label(foreground_input_list_box, text= "or", font = ("Roboto", 14), fg = "#D6D2D2", bg = "#2C2B2B").place(relx= 0.50, rely = 0.45)
-tk.Button(foreground_input_list_box, text = "Browse", height = 1, width=10, font = ("Roboto", 14),  fg = "#D6D2D2", bg = "#2C2B2B", cursor = "hand2", command= get_input_handler).place(relx= 0.45, rely = 0.55)
+foreground_input_list_box.place(relx= 0, rely=0)
+tk.Label(foreground_input_list_box, text= "Drop Input Folder Here", font = ("Roboto", 20), fg = "#D6D2D2", bg = "#2C2B2B").place(relx= 0.35, rely = 0.35)
+tk.Label(foreground_input_list_box, text= "or", font = ("Roboto", 20), fg = "#D6D2D2", bg = "#2C2B2B").place(relx= 0.49, rely = 0.45)
+tk.Button(foreground_input_list_box, text = "Browse", height = 1, width=10, font = ("Roboto", 14),  fg = "#D6D2D2", bg = "#2C2B2B", cursor = "hand2", command= get_input_handler).place(relx= 0.44, rely = 0.55)
 
 #create menu frame widget
 menu_frame = tk.Frame(mainwindow, height= 720, width=350, relief="groove", bg = "#2C2B2B")
