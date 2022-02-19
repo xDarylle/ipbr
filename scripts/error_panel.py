b77a76412255da2af0052c5f9a8eeda928f9d717
xShiro2 <daryllejumalon16@gmail.com>
2022-02-19 10:23:32 +0800

    update: add icons

--- scripts/error_panel.py
+++ scripts/error_panel.py
@@ -1,14 +1,34 @@
 import tkinter as tk
 
-def error_handler(error):
+error_icon = "resources/images/error.ico"
+warning_icon = "resources/images/warning.ico"
+done_icon = "resources/images/done.ico"
+
+def error_handler(error, isError):
     error_window_width = 400
     error_window_height = 150
     error_window = tk.Toplevel()
     error_window.geometry(f"{error_window_width}x{error_window_height}+{int((error_window.winfo_screenwidth() / 2) - (error_window_width / 2))}+{int((error_window.winfo_screenheight() / 2) - (error_window_height - 2))}")
-    error_window.title("ERROR!")
+    if isError:
+        error_window.iconbitmap(error_icon)
+        error_window.title("ERROR!")
+    else:
+        error_window.title("WARNING!")
+        error_window.iconbitmap(warning_icon)
     error_window.configure(bg = "#383737")
 
-    label = tk.Label(error_window, text= error, font = ("Roboto", 15), fg = "#ffffff", bg = "#383737")
+    label = tk.Label(error_window, text= error, font = ("Roboto", 15), fg = "#f0eee9", bg = "#383737")
     label.place(relx=0.5, rely=0.5, anchor='center')
 
+def done_handler(text):
+    error_window_width = 400
+    error_window_height = 150
+    error_window = tk.Toplevel()
+    error_window.geometry(f"{error_window_width}x{error_window_height}+{int((error_window.winfo_screenwidth() / 2) - (error_window_width / 2))}+{int((error_window.winfo_screenheight() / 2) - (error_window_height - 2))}")
+    error_window.title("Notification")
+    error_window.iconbitmap(done_icon)
+    error_window.configure(bg = "#383737")
+
+    label = tk.Label(error_window, text= text, font = ("Roboto", 15), fg = "#f0eee9", bg = "#383737")
+    label.place(relx=0.5, rely=0.5, anchor='center')
 
